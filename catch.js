35e82566041217b1ed27479462f99fd83f2fd4fa
Josef Wittmann <josefwittmann42@gmail.com>
2019-05-11 03:13:34 +0200

    Added Chasing scene

--- catch.js
+++ catch.js
@@ -8,23 +8,36 @@ window.onload = function() {
 		frameCount = 0;
 	context.translate(width*.5, height*.5);
 		
-	var player = new Player([0,0], [1,0.5], .1, 0),
-		boundary = new BoundaryRamp(-height*.5,width*.5,height*.5,-width*.5, Math.min(width, height)*.2, .05);
+	var chaser = new Player([0,0], [0,0], .1, 0, "#ff0000"),
+		runner = new Player([width*.3, 0], [0,3], .1, 0, "#0000ff"),
+		boundary = new BoundaryRamp(-height*.5,width*.5,height*.5,-width*.5, 
+									Math.min(width, height)*.2, .05);
 		
 	render();
 	
 	function render() {
 		context.clearRect(-width*.5,-height*.5,width,height);
 		boundary.draw(context);
-		player.draw(context);
+		chaser.draw(context);
+		runner.draw(context);
 		
-		player.setHeadingTowards(mouseX, mouseY);
-		player.accelerate(.1);
-		player.applyRamp(boundary);
+		chaser.setHeadingTowards(...runner.position);
+		chaser.accelerate(.1);
+		chaser.applyRamp(boundary);
 		
-		player.update();
-		if(frameCount % 15 == 0)
-			player.recordHistory();
+		runner.setHeadingTowards(
+			- chaser.position[0] + 2*runner.position[0],
+			- chaser.position[1] + 2*runner.position[1]);
+		runner.accelerate(.1);
+		runner.applyRamp(boundary);
+		
+		chaser.update();
+		runner.update();
+		
+		if(frameCount % 15 == 0) {
+			chaser.recordHistory();
+			runner.recordHistory();
+		}
 		frameCount++;
 		requestAnimationFrame(render);
 	}
@@ -41,13 +54,15 @@ class Player {
 	speed is a vector of the current speed per frame
 	strength is the possible acceleration in speed per frame	
 	heading is an angle in radians
+	color has to be 6-digit hex for history to work
 	*/
-	constructor(position, speed, strength, heading) {
+	constructor(position, speed, strength, heading, color = "#000000") {
 		this.position = position;
 		this.speed = speed;
 		this.strength = strength;
 		this.heading = heading;
-		this.dragCoeff = 0.01;
+		this.dragCoeff = 0.005;
+		this.color = color;
 		
 		this.histori = Array(10);
 	}
@@ -87,31 +102,39 @@ class Player {
 	
 	recordHistory() {
 		this.histori.shift();
-		this.histori.push(new Player(Array.from(this.position), Array.from(this.speed), this.strength, this.heading));
+		this.histori.push(new Player(Array.from(this.position), Array.from(this.speed), this.strength, this.heading, this.color));
 	}
 	
 	draw(context2d) {
+		this.drawArrow(context2d);
+		this.drawHistory(context2d);
+	}
+	
+	drawArrow(context2d, color = this.color) {
 		context2d.save();
 		context2d.translate(this.position[0], this.position[1]);
 		context2d.rotate(this.heading);
 		
+		context2d.strokeStyle = color;
 		context2d.beginPath();
-		context2d.moveTo(-10, 0);
-		context2d.lineTo(10, 0);
-		context2d.moveTo(10, 0);
-		context2d.lineTo(0, 10);
-		context2d.moveTo(10, 0);
-		context2d.lineTo(0, -10);
+		context2d.moveTo(-20, 0);
+		context2d.lineTo(0, 0);
+		context2d.moveTo(0, 0);
+		context2d.lineTo(-10, 10);
+		context2d.moveTo(0, 0);
+		context2d.lineTo(-10, -10);
 		context2d.stroke();
 		
 		context2d.restore();
-		
+	}
+	
+	drawHistory(context2d) {
 		for(var i=0; i<this.histori.length; i++) {
-			context2d.strokeStyle = "rgba(0,0,0,"+ i/this.histori.length +")";
+			var transparency = "" + Math.round(9*i/this.histori.length)
+			var color = this.color + transparency + transparency
 			if(this.histori[i] != null)
-				this.histori[i].draw(context2d);
+				this.histori[i].drawArrow(context2d, color);
 		}
-		context2d.fillStyle = "black";
 	}
 }
 
@@ -124,6 +147,9 @@ class BoundaryRamp {
 		this.width = width;
 		this.strength = strength;
 		this.maxStrength = width/30;
+		this.colors = {"outside": "#fa8", 
+						"middle": "#fca", 
+						"inside": "white"};
 	}
 	
 	// calculate the acceleration by distance to the edge
@@ -170,19 +196,19 @@ class BoundaryRamp {
 	
 	prepareLinearGradient(context2d, x0, y0, x1, y1) {
 		var grd = context2d.createLinearGradient(x0, y0, x1, y1);
-		grd.addColorStop(0, "grey");
-		grd.addColorStop(.25, "lightgrey");
-		grd.addColorStop(1, "white");
+		grd.addColorStop(0, this.colors["outside"]);
+		grd.addColorStop(.25, this.colors["middle"]);
+		grd.addColorStop(1, this.colors["inside"]);
 		context2d.fillStyle = grd;
 	}
 	
 	prepareRaidalGradient(context2d, centerX, centerY) {
 		var grd = context2d.createRadialGradient(centerX, centerY, 0,
 											 centerX, centerY, this.width*Math.sqrt(2));
-		grd.addColorStop(1, "grey");
-		grd.addColorStop(0.5*Math.sqrt(2), "grey");
-		grd.addColorStop(0.375*Math.sqrt(2), "lightgrey");
-		grd.addColorStop(0, "white");
+		grd.addColorStop(1, this.colors["outside"]);
+		grd.addColorStop(0.5*Math.sqrt(2), this.colors["outside"]);
+		grd.addColorStop(0.375*Math.sqrt(2), this.colors["middle"]);
+		grd.addColorStop(0, this.colors["inside"]);
 		context2d.fillStyle = grd;
 	}
 }
